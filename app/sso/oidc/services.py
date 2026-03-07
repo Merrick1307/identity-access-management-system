e2f29de8082bcf0fb4b931cb6816e931c3d72ca8
Merrick1307 <96961414+Merrick1307@users.noreply.github.com>
2026-03-07 12:23:46 +0100

    add debug logging to oidc endpoints, fix entrypoint command

--- app/sso/oidc/services.py
+++ app/sso/oidc/services.py
@@ -105,7 +105,7 @@ class OIDCService:
                 FROM authorization_codes
                 WHERE code = $1 \
                   AND client_id = $2 \
-                  AND redirect_uri = $3
+                  AND redirect_uri = $3 \
                   AND used = FALSE \
                   AND expires_at > NOW() \
                 """
